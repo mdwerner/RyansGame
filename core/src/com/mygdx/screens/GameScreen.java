e8bd2958cd5bf70146398a94a95cd6743a9640eb
mdphysics <mdwerner@iastate.edu>
2017-01-18 20:28:03 -0600

    Added Rendering and World classes
    I added some basic rendering and world classes and implemented them in
    the game screen class.  At this point it is simply a rectangle that
    scrolls across the screen.

---
+++ core/src/com/mygdx/screens/GameScreen.java
@@ -0,0 +1,61 @@
+package com.mygdx.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.mygdx.gameworld.GameRenderer;
+import com.mygdx.gameworld.GameWorld;
+
+public class GameScreen implements Screen {
+
+	private GameWorld world;
+	private GameRenderer renderer;
+	
+	public GameScreen() {
+        Gdx.app.log("GameScreen", "Attached");
+        world = new GameWorld();
+        renderer = new GameRenderer(world);
+    }
+
+    @Override
+    public void render(float delta) {
+        // Sets a Color to Fill the Screen with (RGB = 10, 15, 230), Opacity of 1 (100%)
+        Gdx.gl.glClearColor(10/255.0f, 15/255.0f, 230/255.0f, 1f);
+        // Fills the screen with the selected color
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.app.log("GameScreen FPS", (1/delta) + "");
+        
+        world.update(delta); //Update the world
+        renderer.render();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        Gdx.app.log("GameScreen", "resizing");
+    }
+
+    @Override
+    public void show() {
+        Gdx.app.log("GameScreen", "show called");
+    }
+
+    @Override
+    public void hide() {
+        Gdx.app.log("GameScreen", "hide called");     
+    }
+
+    @Override
+    public void pause() {
+        Gdx.app.log("GameScreen", "pause called");        
+    }
+
+    @Override
+    public void resume() {
+        Gdx.app.log("GameScreen", "resume called");       
+    }
+
+    @Override
+    public void dispose() {
+        // Leave blank
+    }
+}
