2aa38f2c4165a2c39d6e136bd0660b394ad562f2
mdphysics <mdwerner@iastate.edu>
2017-02-08 19:06:04 -0600

    Revert "Added Rendering and World classes"
    This reverts commit e8bd2958cd5bf70146398a94a95cd6743a9640eb.
    
    # Conflicts:
    #	core/src/com/mygdx/gameworld/GameRenderer.java
    #	core/src/com/mygdx/gameworld/GameWorld.java
    #	core/src/com/mygdx/screens/GameScreen.java

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,20 +1,33 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.mygdx.screens.GameScreen;
 
-public class MyGdxGame extends Game {
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
 
 	@Override
-	public void create() {
-		// TODO Auto-generated method stub
-		Gdx.app.log("ZBGame", "created");
-		setScreen(new GameScreen());
-	}//end create function
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
 	
-}//end MyGdxGame class
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
