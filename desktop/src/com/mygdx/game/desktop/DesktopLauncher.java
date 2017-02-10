99297c50b81c1c64d9567ececa174b44717359d9
mdphysics <mdwerner@iastate.edu>
2017-02-09 19:23:40 -0600

    Cameras & Scenes
    I've included example code on how to use Camera objects and Scenes
    (Scenes hold all of the "actors" in a game world and also allow overlays
    like UIs).

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -3,14 +3,22 @@ package com.mygdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.AnimationDemo;
+import com.mygdx.game.CameraDemo;
 import com.mygdx.game.InputDemo;
 import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.SceneDemo;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		//new LwjglApplication(new MyGdxGame(), config);
 		//new LwjglApplication(new AnimationDemo(), config);
-		new LwjglApplication(new InputDemo(), config);
+		//new LwjglApplication(new InputDemo(), config);
+		config.title = "camera";
+		config.width = 1280; //This changes the resolution of the game screen
+		config.height = 720;
+		
+		//new LwjglApplication(new CameraDemo(), config);
+		new LwjglApplication(new SceneDemo(), config);
 	}
 }
